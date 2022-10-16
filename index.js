7df3091b9906347f2a8c7c369a3e22da0ac08402
Prawee <prawinn555@gmail.com>
2022-10-16 14:38:36 +0200

    demo app

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const multer = require('multer');
+const upload = multer();
+
+// multer : form must be enctype="multipart/form-data"
+// otherwise, it is possible to use bodyParser (urlEncoded or json)
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/logViewer', (req, res) => res.send('method must be POST'))
+  .post('/logViewer', upload.none(), (req, res) => {
+    console.log('logViewer', req.body);
+    res.render('pages/logViewer', { body : req.body})}
+  )
+ .listen(PORT, () => console.log(`Listening on ${ PORT }`))
